be8b9b1c7c40f3b564cf472c2cfd1a67387a09a6
djangojazz <brettmorin2@gmail.com>
2017-11-25 15:09:55 -0800

    Made change to migration to manually create a procedure

--- EFCoreCodeFirstScaffolding/Migrations/20171125223203_AddedColumn.cs
+++ EFCoreCodeFirstScaffolding/Migrations/20171125223203_AddedColumn.cs
@@ -13,6 +13,9 @@ namespace EFCoreCodeFirstScaffolding.Migrations
                 table: "Aircraft",
                 nullable: false,
                 defaultValue: 0);
+
+            //There is not direct Create Procedure that I could see directly.
+            migrationBuilder.Sql("create proc pGiveBack ( @Input varchar(128) ) as Begin Select @input as Output End");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -20,6 +23,8 @@ namespace EFCoreCodeFirstScaffolding.Migrations
             migrationBuilder.DropColumn(
                 name: "TestId",
                 table: "Aircraft");
+
+            migrationBuilder.Sql("drop proc pGiveBack");
         }
     }
 }
